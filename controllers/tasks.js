65fd7a7d029ad11d6149b884032bb771e0ba2d57
Aquib Faiyaz <faiyazaquib28@gmail.com>
2021-10-29 11:20:29 +0530

    delete task setup

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,3 +1,4 @@
+const { findOneAndDelete } = require("../models/Task");
 const Task = require("../models/Task");
 
 const getAllTasks = async (req, res) => {
@@ -31,12 +32,24 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("task updated");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `no task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("task deleted");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    res.status(200).json({});
+  } catch (error) {}
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
